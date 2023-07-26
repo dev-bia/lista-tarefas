cbaabea19522e699ab0c2cfc479e194fdae9ba9b
Beatriz Araújo <b.cordeiroaraujo@gmail.com>
2023-07-25 21:54:25 -0300

    Segundo commit

--- js/controle.js
+++ js/controle.js
@@ -5,7 +5,7 @@ let main = document.getElementById('areaLista');
 let contador = 0;
 
 function addTarefa() {
-    //Pegar o valor digitado no input`
+    //Pegar o valor digitado no input
     let valorInput = input.value;
 
     //Se não for vazio, nem nulo e nem indefinido:
@@ -13,19 +13,20 @@ function addTarefa() {
 
         ++contador;
 
-        let novoItem = `<div id="${contador}" class="item">
-        <div onclick="marcarTarefa(${contador})" class="item-icone">
-            <i id="icone_${contador}" class="mdi mdi-circle-outline"></i>
-        </div>
-        <div onclick="marcarTarefa(${contador})" class="item-nome">
+        let novoItem = 
+        `<div id="${contador}" class="item">
+            <div onclick="marcarTarefa(${contador})" class="item-icone">
+                <i id="icone_${contador}" class="mdi mdi-circle-outline"></i>
+            </div>
+            <div onclick="marcarTarefa(${contador})" class="item-nome">
             ${valorInput}
-        </div>
-        <div class="item-botao">
+            </div>
+            <div class="item-botao">
             <button onclick="deletar(${contador})"  class="delete"><i class="mdi mdi-delete"></i>
                 Deletar
             </button>
-        </div>
-    </div>`;
+            </div>
+        </div>`;
         //Adicionar novo item no main
         main.innerHTML += novoItem;
         //Limpar o input
@@ -39,14 +40,14 @@ function deletar(id) {
     tarefa.remove();
 }
 
-function marcarTarefa(id){
+function marcarTarefa(id) {
     var item = document.getElementById(id);
     var classe = item.getAttribute('class');
 
-    if(classe=="item"){
+    if (classe == "item") {
         item.classList.add('clicado');
 
-        var icone = document.getElementById('icone_' +id);
+        var icone = document.getElementById('icone_' + id);
         icone.classList.remove('mdi-circle-outline');
         icone.classList.add('mdi-check-circle');
 
@@ -55,13 +56,13 @@ function marcarTarefa(id){
     } else {
         item.classList.remove('clicado');
 
-        var icone = document.getElementById('icone_' +id);
+        var icone = document.getElementById('icone_' + id);
         icone.classList.remove('mdi-check-circle');
         icone.classList.add('mdi-circle-outline');
     }
 }
 
-//Para disparar nova tarefda apertando o "enter"
+//Para disparar nova tarefa apertando o "enter"
 input.addEventListener("keyup", function (event) {
     //se teclou a tecla 13 = enter
     if (event.keyCode === 13) {
